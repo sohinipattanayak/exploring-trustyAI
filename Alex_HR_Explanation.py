55728a16f632540fffcd258596ad3dd838e59eea
sohinipattanayak <sohinipattanayak.it@gmail.com>
2023-10-26 11:34:26 +0530

    Training a model

--- Alex_HR_Explanation.py
+++ Alex_HR_Explanation.py
@@ -10,3 +10,6 @@ X = data.drop(columns=["Candidate Name", "Hired"])
 y = data["Hired"]
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+
+# Train a logistic regression model
+clf = LogisticRegression(max_iter=1000).fit(X_train, y_train)
